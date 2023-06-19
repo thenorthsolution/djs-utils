81136fac86475a5fd3f63f3c7cb7675972bdeef3
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-06-19 09:56:42 +0800

    better

--- test/modules/giveaways.js
+++ test/modules/giveaways.js
@@ -3,7 +3,9 @@ import { RecipleClient, SlashCommandBuilder } from "reciple";
 import { GiveawayManager, JsonDatapaseAdapter } from '@falloutstudios/djs-giveaways';
 import path from "path";
 import { fileURLToPath } from "url";
-import { userMention } from "discord.js";
+import { ChatInputCommandInteraction, userMention } from "discord.js";
+import { InteractionListenerType } from "reciple-interaction-events";
+import anticrash from "./utils/anticrash.js";
 
 // @ts-check
 
@@ -15,103 +17,90 @@ export class Giveaways {
     giveaways = null;
     commands = [
         new SlashCommandBuilder()
-            .setName('start-giveaway')
-            .setDescription('Start a new giveaway')
-            .addStringOption(name => name
-                .setName('name')
-                .setDescription('The giveaway name (Giveaway prize)')
-                .setRequired(true)
+            .setName('giveaway')
+            .setDescription('Manage giveaways')
+            .addSubcommand(start => start
+                .setName('start')
+                .setDescription('Start a new giveaway')
+                .addStringOption(name => name
+                    .setName('name')
+                    .setDescription('The giveaway name (Giveaway prize)')
+                    .setRequired(true)
+                )
+                .addStringOption(duration => duration
+                    .setName('duration')
+                    .setDescription('Giveaway duration')
+                    .setRequired(true)
+                )
+                .addNumberOption(winners => winners
+                    .setName('winners')
+                    .setDescription('Number of winners')
+                    .setRequired(true)
+                )
             )
-            .addStringOption(duration => duration
-                .setName('duration')
-                .setDescription('Giveaway duration')
-                .setRequired(true)
+            .addSubcommand(end => end
+                .setName('end')
+                .setDescription('Ends a giveaway')
+                .addStringOption(giveaway => giveaway
+                    .setName('giveaway')
+                    .setDescription('The giveaway you want to end')
+                    .setAutocomplete(true)
+                    .setRequired(true)
+                )
+                .addBooleanOption(cancel => cancel
+                    .setName('cancel')
+                    .setDescription('End giveaway without choosing winners')
+                )
             )
-            .addNumberOption(winners => winners
-                .setName('winners')
-                .setDescription('Number of winners')
-                .setRequired(true)
-            )
-            .setExecute(async ({ interaction }) => {
-                const name = interaction.options.getString('name', true);
-                const duration = ms(interaction.options.getString('duration', true));
-                const winners = interaction.options.getNumber('winners', true);
-
-                await interaction.deferReply({ ephemeral: true });
-
-                const message = await this.giveaways.createGiveaway({
-                    channel: interaction.channel,
-                    endsAt: duration,
-                    name,
-                    winnerCount: winners
-                });
-
-                await interaction.editReply(message.url);
-            }),
-        new SlashCommandBuilder()
-            .setName('end-giveaway')
-            .setDescription('Ends a giveaway')
-            .addStringOption(giveaway => giveaway
-                .setName('giveaway')
-                .setDescription('The giveaway you want to end')
-                .setRequired(true)
-            )
-            .addBooleanOption(cancel => cancel
-                .setName('cancel')
-                .setDescription('End giveaway without choosing winners')
-            )
-            .setExecute(async ({ interaction }) => {
-                const giveawayId = interaction.options.getString('giveaway', true);
-                const cancel = interaction.options.getBoolean('cancel') || false;
-
-                await interaction.deferReply({ ephemeral: true });
-
-                const giveaway = (await this.giveaways.databaseAdapter.fetchGiveaways({ filter: { messageId: giveawayId } }))[0];
-                if (!giveaway) {
-                    await interaction.editReply(`Giveaway not found`);
-                    return;
-                }
-
-                await this.giveaways.endGiveaway(giveaway.id, !cancel);
-                await interaction.editReply(`Ended giveaway`);
-            }),
-        new SlashCommandBuilder()
-            .setName('reroll-giveaway')
-            .setDescription('Rerolls giveaway winners')
-            .addStringOption(giveaway => giveaway
-                .setName('giveaway')
-                .setDescription('The giveaway you want to end')
-                .setRequired(true)
+            .addSubcommand(reroll => reroll
+                .setName('reroll')
+                .setDescription('Rerolls giveaway winners')
+                .addStringOption(giveaway => giveaway
+                    .setName('giveaway')
+                    .setDescription('The giveaway you want to end')
+                    .setAutocomplete(true)
+                    .setRequired(true)
+                )
             )
             .setExecute(async ({ interaction }) => {
-                const giveawayId = interaction.options.getString('giveaway', true);
-                const cancel = interaction.options.getBoolean('cancel') || false;
+                const subcommmand = interaction.options.getSubcommand(true);
 
-                await interaction.deferReply({ ephemeral: true });
-
-                const giveaway = (await this.giveaways.databaseAdapter.fetchGiveaways({ filter: { messageId: giveawayId } }))[0];
-                if (!giveaway) {
-                    await interaction.editReply(`Giveaway not found`);
-                    return;
+                switch (subcommmand) {
+                    case 'start': return this.handleGiveawayStartCommand(interaction);
+                    case 'end': return this.handleGiveawayEndCommand(interaction);
+                    case 'reroll': return this.handleGiveawayRerollCommand(interaction);
                 }
-
-                const winners = await this.giveaways.getRandomGiveawayEntries(giveaway.id, giveaway.winnerCount);
-                const message = await this.giveaways.getGiveawayMessage(giveaway);
-
-                if (!winners.selected.length) {
-                    await interaction.editReply(`No winners selected from reroll`);
-                    return;
-                }
-
-                await message.reply(`${winners.selected.map(e => userMention(e.userId)).join('')} won the reroll!`);
-                await interaction.editReply(`Reroll successfull`);
             })
     ];
 
     /**
+     * @type {import("reciple-interaction-events").AnyInteractionListener[]}
+     */
+    interactionListeners = [
+        {
+            type: InteractionListenerType.Autocomplete,
+            commandName: 'giveaway',
+            execute: async interaction => {
+                if (!interaction.inGuild()) return;
+
+                const query = interaction.options.getFocused();
+                const ended = interaction.options.getSubcommand() === 'reroll';
+
+                let giveaways = await this.giveaways.databaseAdapter.fetchGiveaways({
+                    filter: { guildId: interaction.guildId, ended }
+                });
+
+                giveaways = query ? giveaways.filter(g => g.name.toLowerCase().includes(query.toLowerCase())) : giveaways;
+                giveaways = giveaways.splice(0, 20);
+
+                await interaction.respond(giveaways.map(g => ({ name: `(${g.messageId}) ${g.name}`, value: g.messageId })));
+            }
+        }
+    ];
+
+    /**
      * 
-     * @param {RecipleClient} client 
-     * @returns 
+     * @param {RecipleClient} client
      */
     onStart(client) {
         this.giveaways = new GiveawayManager({
@@ -130,13 +119,83 @@ export class Giveaways {
 
     /**
      * 
-     * @param {RecipleClient} client 
+     * @param {RecipleClient} client
      */
     async onLoad(client) {
-        this.giveaways.on('error', err => client.logger.error(err));
+        this.giveaways.on('error', err => anticrash.default.report(err));
 
         await this.giveaways.start();
         await this.giveaways.clean();
+    }
+
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    async handleGiveawayStartCommand(interaction) {
+        const name = interaction.options.getString('name', true);
+        const duration = ms(interaction.options.getString('duration', true));
+        const winners = interaction.options.getNumber('winners', true);
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const message = await this.giveaways.createGiveaway({
+            channel: interaction.channel,
+            endsAt: duration,
+            name,
+            winnerCount: winners
+        });
+
+        await interaction.editReply(message.url);
+    }
+
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @returns 
+     */
+    async handleGiveawayEndCommand(interaction) {
+        const giveawayId = interaction.options.getString('giveaway', true);
+        const cancel = interaction.options.getBoolean('cancel') || false;
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const giveaway = (await this.giveaways.databaseAdapter.fetchGiveaways({ filter: { messageId: giveawayId } }))[0];
+        if (!giveaway) {
+            await interaction.editReply(`Giveaway not found`);
+            return;
+        }
+
+        await this.giveaways.endGiveaway(giveaway.id, !cancel);
+        await interaction.editReply(`Ended giveaway`);
+    }
+
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @returns 
+     */
+    async handleGiveawayRerollCommand(interaction) {
+        const giveawayId = interaction.options.getString('giveaway', true);
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const giveaway = (await this.giveaways.databaseAdapter.fetchGiveaways({ filter: { messageId: giveawayId } }))[0];
+        if (!giveaway) {
+            await interaction.editReply(`Giveaway not found`);
+            return;
+        }
+
+        const winners = await this.giveaways.getRandomGiveawayEntries(giveaway.id, giveaway.winnerCount);
+        const message = await this.giveaways.getGiveawayMessage(giveaway);
+
+        if (!winners.selected.length) {
+            await interaction.editReply(`No winners selected from reroll`);
+            return;
+        }
+
+        await message.reply(`${winners.selected.map(e => userMention(e.userId)).join('')} won the reroll!`);
+        await interaction.editReply(`Reroll successfull`);
     }
 };
 
