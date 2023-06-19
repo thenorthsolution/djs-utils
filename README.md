2b368581964703161a5b92dc1e07a1be25918a73
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-06-19 10:03:31 +0800

    Better embed timer

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 A monorepo of utility packages for Discord.js
 
 # Packages
+- @falloutstudios/djs-giveaways
 - @falloutstudios/djs-pagination
 - @falloutstudios/djs-scam-links
--- packages/giveaways/src/classes/GiveawayManager.ts
+++ packages/giveaways/src/classes/GiveawayManager.ts
@@ -123,7 +123,7 @@ export class GiveawayManager extends TypedEmitter<GiveawayManagerEvents> {
         embed.setTitle(giveaway.name);
         embed.setAuthor({ name: '🎉 Giveaway' });
         embed.setColor(giveaway.ended ? 'DarkButNotBlack' : 'Blue');
-        embed.addFields({ name: `${inlineCode('⏲️')} Ends`, value: time(giveaway.endsAt) });
+        embed.addFields({ name: `${inlineCode('⏲️')} End${giveaway.ended ? 'ed' : 's'}`, value: time(giveaway.endsAt, 'R') + (!giveaway.ended ? `(${time(giveaway.endsAt)})` : '') });
         embed.addFields({ name: `${inlineCode('👥')} Entrie${(giveaway.entries ?? 0) > 1 ? 's' : ''}`, value: `${giveaway.entries ? inlineCode(giveaway.entries.toLocaleString('en-US')) : '**No entries**'}` });
         embed.setFooter({ text: giveaway.ended ? 'Ended' : 'Active' });
         embed.setTimestamp(giveaway?.endedAt ?? giveaway.createdAt);
