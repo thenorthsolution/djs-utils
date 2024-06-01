30d231d9679d9fe0da3d3e958116fcdbd962ec1f
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-06-01 21:17:04 +0800

    update package data

--- test/modules/utils/anticrash.js
+++
@@ -1,3 +0,0 @@
-import { RecipleAnticrash } from 'reciple-anticrash';
-
-export default new RecipleAnticrash();
--- test/modules/utils/interactionEvents.js
+++
@@ -1,3 +0,0 @@
-import { InteractionEventManager } from 'reciple-interaction-events';
-
-export default new InteractionEventManager();
