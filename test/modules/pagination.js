6835ad45c0485a8795736ff8e271306794f86789
Cat++ <69035887+NotGhex@users.noreply.github.com>
2024-06-01 22:30:34 +0800

    pagination buttons

--- test/modules/pagination.js
+++ test/modules/pagination.js
@@ -1,6 +1,6 @@
 // @ts-check
-import { ButtonPaginationBuilder, SendAs } from '@thenorthsolution/djs-pagination';
-import { EmbedBuilder } from 'discord.js';
+import { ButtonPaginationBuilder, PaginationControllerType, SendAs } from '@thenorthsolution/djs-pagination';
+import { ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js';
 import { SlashCommandBuilder } from "reciple";
 
 export class Pagination {
@@ -14,12 +14,18 @@ export class Pagination {
             .setExecute(async ({ interaction }) => {
                 const pagination = new ButtonPaginationBuilder();
 
-                pagination.setPages(
-                    { content: 'Content', embeds: [{ title: 'Embed' }] },
-                    'Just Content',
-                    new EmbedBuilder().setTitle('Just Embed'),
-                    () => `Dynamic ${new Date().toISOString()}`
-                )
+                pagination
+                    .setButtons(
+                        { button: new ButtonBuilder().setCustomId('prev').setLabel('Previous').setStyle(ButtonStyle.Secondary), type: PaginationControllerType.PreviousPage },
+                        { button: new ButtonBuilder().setCustomId('stop').setLabel('Stop').setStyle(ButtonStyle.Danger), type: PaginationControllerType.Stop },
+                        { button: new ButtonBuilder().setCustomId('next').setLabel('Next').setStyle(ButtonStyle.Secondary), type: PaginationControllerType.NextPage },
+                    )
+                    .setPages(
+                        { content: 'Content', embeds: [{ title: 'Embed' }] },
+                        'Just Content',
+                        new EmbedBuilder().setTitle('Just Embed'),
+                        () => `Dynamic ${new Date().toISOString()}`
+                    )
 
                 await pagination.send({
                     command: interaction,
